1204ed9d0ac6255daf55143c42134c334783cfea
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio>
2025-03-30 22:33:50 -0600

    Handling errors.

---
+++ src/app/meals/error.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>An error ocurred!</h1>
+      <p>Failed to fetch meal data</p>
+    </main>
+  );
+};
+
+export default Error;
