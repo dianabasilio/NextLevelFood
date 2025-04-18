f9425d0d3b6da72830fad3259fa17c9b7b07c506
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio>
2025-04-17 21:20:06 -0600

    just get params.

--- src/app/meals/[slug]/page.jsx
+++ src/app/meals/[slug]/page.jsx
@@ -1,54 +1,11 @@
-import React, { Suspense } from "react";
-import Image from "next/image";
-import { getMeal } from "@/lib/meals";
+import React from "react";
+
 import styles from "./page.module.scss";
-import { notFound } from "next/navigation";
-
-// Componente para cargar y mostrar los detalles de una comida
-const MealDetails = async ({ slug }) => {
-  const meal = await getMeal(slug);
-
-  if (!meal) {
-    notFound();
-  }
-
-  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
-
-  return (
-    <>
-      <header className={styles.header}>
-        <div className={styles.image}>
-          <Image
-            src={meal.image}
-            alt={meal.title}
-            fill
-            style={{ objectFit: "cover" }}
-          />
-        </div>
-        <div className={styles.headerText}>
-          <h1>{meal.title}</h1>
-          <p className={styles.creator}>
-            by <a href={`mailto:${meal.creator_email}`}> {meal.creator}</a>
-          </p>
-          <p className={styles.summary}> {meal.summary}</p>
-        </div>
-      </header>
-
-      <main>
-        <p
-          className={styles.instructions}
-          dangerouslySetInnerHTML={{
-            __html: meal.instructions,
-          }}
-        />
-      </main>
-    </>
-  );
-};
 
 // Componente de página para la ruta dinámica /meals/[slug]
 const MealsSlugpage = async ({ params }) => {
   console.log("params.slug", params.slug);
+  return <main className={styles.main}></main>;
 };
 
 export default MealsSlugpage;
