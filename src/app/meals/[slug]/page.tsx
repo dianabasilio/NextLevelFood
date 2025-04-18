c5702feda7c03ad80e7795d46f170677d1601e26
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio>
2025-04-17 22:24:22 -0600

    prueba

--- src/app/meals/[slug]/page.tsx
+++ src/app/meals/[slug]/page.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import Image from "next/image";
-import { getMeal } from "@/lib/meals";
-import { Meal } from "@/types/types";
+
 import styles from "./page.module.scss";
-import { notFound } from "next/navigation";
 import { NextPage } from "next";
 
 interface MealsSlugpageParams {
@@ -17,39 +14,10 @@ interface MealsSlugpageProps {
 const MealsSlugpage: NextPage<MealsSlugpageProps> = async ({ params }) => {
   const { slug } = await params;
   console.log("params.slug", slug);
-  const meal: Meal = getMeal(slug);
-
-  if (!meal) {
-    notFound();
-  }
-
-  if (meal) {
-    meal.instructions = meal.instructions.replace(/\n/g, "<br />");
-  }
 
   return (
     <>
-      <header className={styles.header}>
-        <div className={styles.image}>
-          <Image src={meal.image} alt="some image" fill />
-        </div>
-        <div className={styles.headerText}>
-          <h1>{meal.title}</h1>
-          <p className={styles.creator}>
-            by <a href={`mailto:${meal.creator_email}`}> {meal.creator}</a>
-          </p>
-          <p className={styles.summary}> {meal.summary}</p>
-        </div>
-      </header>
-
-      <main>
-        <p
-          className={styles.instructions}
-          dangerouslySetInnerHTML={{
-            __html: meal.instructions,
-          }}
-        />
-      </main>
+      <header className={styles.header}></header>
     </>
   );
 };
