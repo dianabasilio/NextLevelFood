3a98481fd6348ab107921a753e37d0313c84fed9
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio>
2025-04-17 20:31:09 -0600

    Meal not found.

--- src/app/meals/not-found.js
+++ src/app/meals/not-found.js
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
   return (
     <main className="not-found">
-      <h1>Meal found</h1>
+      <h1>Meal not found</h1>
       <p>Unfortunately, we could not find the requested page or meal.</p>
     </main>
   );
