b1a699a634619244216946e3c59efeb7acd1be29
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio>
2025-04-17 23:01:09 -0600

    prueba js.

--- src/app/meals/[slug]/page.js
+++ src/app/meals/[slug]/page.js
@@ -1,16 +1,7 @@
 import React from "react";
-
 import styles from "./page.module.scss";
 
-interface MealsSlugpageParams {
-  slug: string;
-}
-
-interface MealsSlugpageProps {
-  params: MealsSlugpageParams;
-}
-
-const MealsSlugpage: React.FC<MealsSlugpageProps> = async ({ params }) => {
+const MealsSlugpage = ({ params }) => {
   const { slug } = params; // ✅ No necesitas hacer await
   console.log("params.slug", slug);
 
